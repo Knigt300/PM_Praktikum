8211d83e2726f3665b83c77b628fe7e93736dab2
kr1sch1 <krischanbusiness@gmail.com>
2026-03-05 09:31:34 +0100

    A3alle.py

--- A3alle.py
+++ A3alle.py
@@ -9,27 +9,27 @@ def calculate_waiting_times(case: list):
     
     for i in range(len(case)):
 
-        
+        # Letzte Aktivität hat keine Wartezeit
         if i == len(case) - 1:
             case[i]['wait_time'] = zero_time
 
         else:
-            
+            # Endzeit aktuelles Event
             end_current = dt.datetime.strptime(
                 case[i]['end_timestamp'],
                 '%Y-%m-%d %H:%M:%S')
             
 
-            
+            # Startzeit nächstes Event
             start_next = dt.datetime.strptime(
                 case[i+1]['start_timestamp'],
                 '%Y-%m-%d %H:%M:%S')
             
 
-            
+            # Wartezeit
             wait = start_next - end_current
 
-            
+            # Negative Wartezeit verhindern
             if wait > zero_time:
                 case[i]['wait_time'] = wait
             else:
@@ -40,16 +40,16 @@ def calculate_waiting_times(case: list):
                 
 def calculate_case_metrics(case: list):
     ''' Berechnet Kennzahlen für einen einzelnen Case '''
-
+# Summenvariable auf 0 gesetzt
     total_activity_time = dt.timedelta(seconds=0)
     total_wait_time = dt.timedelta(seconds=0)
 
-    
+    # Erste Startzeit
     first_start = dt.datetime.strptime(
         case[0]['start_timestamp'],
-        '%Y-%m-%d %H:%M:%S'
-    )
-
+        '%Y-%m-%d %H:%M:%S')
+    
+    # Späteste Startzeit initialisieren
     last_end = first_start
 
     
@@ -69,11 +69,11 @@ def calculate_case_metrics(case: list):
         # Prüfen, ob diese Aktivität die späteste Endzeit hat
         if end > last_end:
             last_end = end
-
+    # Anzahl Aktivitäten
     num_activities = len(case)
-
+    # Ergebnis Dict.
     result = {}
-
+    # Speicher Resultate, berechnet Durchschnittswerte
     result['activities'] = case
     result['total_duration'] = last_end - first_start
     result['average_activity_duration'] = total_activity_time / num_activities
@@ -87,7 +87,7 @@ def calculate_case_metrics(case: list):
 
 def performance_log(log: dict):
     ''' Berechnet Durchschnittswerte für das gesamte Log '''
-
+    # Gesamtsummen
     total_act_time = dt.timedelta(seconds=0)
     total_wait_time = dt.timedelta(seconds=0)
     total_activities = 0
@@ -105,9 +105,9 @@ def performance_log(log: dict):
             num_act = log[case_id]['num_activities'] 
 
             total_activities += num_act  
-            total_act_time += log[case_id]['average_activity_duration'] * num_act 
-            total_wait_time += log[case_id]['average_wait_time'] * (num_act - 1)
-            total_waits += (num_act - 1)
+            total_act_time += log[case_id]['average_activity_duration'] * num_act # Durchschnitt x Anzahl = Gesamtsumme
+            total_wait_time += log[case_id]['average_wait_time'] * (num_act - 1) # Wartezeit: Durchschnitt x (Anzahl -1)
+            total_waits += (num_act - 1) # Gesamtzeit aller Wartephasen
 
     # Log-Durchschnittswerte speichern
     log['average_activity_time'] = total_act_time / total_activities
@@ -147,29 +147,29 @@ def find_bottlenecks(log: dict):
     return problems
 
 
-
+# Selektiert die längsten Aktivitäten
 def get_longest_acts(log: dict, num=1):
-    acts = log['average_acts']
-
-    longest = []
+    acts = log['average_acts'] # Durchschnittsdaten holen
+    
+    longest = [] # Speicher längster Aktivitäten
 
     
     for activity in acts:
 
-        current = (activity, acts[activity])
+        current = (activity, acts[activity]) # Aktuelles Tupel erstellen
 
-        
+        # Wenn Liste noch nicht voll ist dann wird aktuelles Element hinzugefügt
         if len(longest) < num:
             longest.append(current)
 
         else:
             
-            smallest_index = 0
+            smallest_index = 0 #Startwert erstes Element
             for i in range(len(longest)):
-                if longest[i][1] < longest[smallest_index][1]:
+                if longest[i][1] < longest[smallest_index][1]:   # Kleinstes Element in der Liste suchen
                     smallest_index = i
 
-            
+            # Alles was kleiner als die kleinste gespeicherte Dauer ist wird ignoriert
             if current[1] > longest[smallest_index][1]:
                 longest[smallest_index] = current
 
